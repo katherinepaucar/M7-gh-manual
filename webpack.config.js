52ff464e26c7f48259db838d22bbecd4edae92a1
Katherine Paucar <gaby2091@outlook.es>
2024-03-21 20:44:41 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
   stats: "errors-only",
   output: {
     filename: "[name].[chunkhash].js",
-    publicPath: "./",
+    publicPath: "/",
   },
   devServer: {
     historyApiFallback: true,
